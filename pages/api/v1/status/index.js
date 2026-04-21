ab530088e1db85014935e60a4c6d5776333178d2
Felipe <93229568+lfelipesal@users.noreply.github.com>
2026-04-21 17:44:01 +0000

    Adicionando do status do banco de dados no endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "Tenho muito trabalho pela frente é amores." });
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseUsedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        used_connections: databaseUsedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
